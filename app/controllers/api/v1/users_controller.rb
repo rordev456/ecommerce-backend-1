6b6c65be678c4ee22cb6c745a81235322ed36807
rordev456 <devidev45@proton.me>
2019-02-28 14:06:51 -0500

    Added JWT gem and some auth

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -4,6 +4,34 @@ class Api::V1::UsersController < ApplicationController
         @users = User.all
         render json: @users
     end
+
+    def create
+        @user = User.create(user_params)
+        if @user.valid?
+            @token = encode_token(user_id: @user.id)
+            render json: {user: @user, jwt: @token}, status: :created
+        else render json: { error: "Failed to create user"}, status: :not_acceptable
+        end
+    end
+
+    def login
+        puts user_params
+        @user = User.find_by(email: user_params[:email])
+        if @user
+            if @user.authenticate(user_params[:password])
+                @token = encode_token(user_id: @user.id)
+                render json: {user: @user, jwt: @token}, status: :created
+            else render json: {message: "The password didn't work"}, status: :not_acceptable
+            end
+        else render json: {message: "The email didn't work"}, status: :not_acceptable
+        end
+    end
+    
+
+    private
+    def user_params
+        params.require(:user).permit(:username, :password)        
+    end
     
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,15 @@
 Rails.application.routes.draw do
   namespace 'api' do
     namespace 'v1' do
-  # resources :sizes
-  # resources :inventories
-  resources :categories, only: [:index]
-  # resources :products
-  # resources :product_carts
-  # resources :carts
-  resources :users, only: [:index]
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+      # resources :sizes
+      # resources :inventories
+      resources :categories, only: [:index]
+      # resources :products
+      # resources :product_carts
+      # resources :carts
+      resources :users, only: [:index, :create]
+      post '/login', to: 'users#login'
+      # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
     end
   end
 end
